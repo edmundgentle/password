a3bb9a2e8838c1eca63cb8e61997e5f9651b0f20
Edmund Gentle <edmund@newhaze.com>
2013-03-10 13:32:11 +0000

    Added support for personal information checking

--- src/password.js
+++ src/password.js
@@ -1,6 +1,3 @@
-/*
-
-*/
 (function( $ ){
 	var methods = {
 		init : function( options ) { 
@@ -8,7 +5,8 @@
 				minLength:8,
 				allowSpace:false,
 				change:function() {},
-				strengthIndicator:null
+				strengthIndicator:null,
+				personalInformation:{}
 			}, options);
 			this.each(function() {
 				var t=$(this);
@@ -31,6 +29,13 @@
 						}
 					}
 				}
+				$.each(settings.personalInformation, function(index, value) {
+					if(typeof(value)=='string') {
+						if($(value).length>0) {
+							settings.personalInformation[index]=$(value);
+						}
+					}
+				});
 				t.keyup(function() {
 					var score = $.fn.password('calculateScore',t,settings);
 					if(si!=null) {
@@ -52,7 +57,7 @@
 		calculateScore : function(elem, settings) {
 			var s=elem.val();
 			var score=0;
-			var max_score=50;
+			var max_score=70;
 			var pass=true;
 			var errors=[];
 			if(s.length>=settings.minLength) {
@@ -84,6 +89,32 @@
 				score+=10;
 			}else{
 				errors.push('NO_SPECIAL_CHARACTERS');
+			}
+			//check personal information
+			var pi=[];
+			$.each(settings.personalInformation, function(index, value) {
+				if(typeof(value)=='string') {
+					pi.push(value.toLowerCase());
+					var n=value.match(/[1-2][0-9]{3}/g);
+					if(n) {
+						pi.push(n[0].substring(2,4));
+					}
+				}else{
+					pi.push(value.val().toLowerCase());
+				}
+			});
+			var slc=s.toLowerCase();
+			var fpi=false;
+			$.each(pi, function(index, value) {
+				var regex = new RegExp(value, "g");
+				if(slc.match(regex)!=null) {
+					fpi=true;
+				}
+			});
+			if(fpi) {
+				errors.push('CONTAINS_PERSONAL_INFO');
+			}else if(s.length>5) {
+				score+=20;
 			}
 			if((score/max_score)<0.3) {
 				pass=false;
