031737d820cda48e1544b263d22a8c1408f4b094
Edmund Gentle <edmundgentle@yahoo.co.uk>
2015-04-22 13:13:05 +0100

    Removed $.error

--- src/jquery.password.js
+++ src/jquery.password.js
@@ -251,8 +251,6 @@
 			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
 		} else if ( typeof method === 'object' || ! method ) {
 			return methods.init.apply( this, arguments );
-		}else{
-			$.error( 'Method ' +  method + ' does not exist on jQuery.password' );
 		}
 	};
 })( jQuery );
